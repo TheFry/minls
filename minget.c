1b643d03f8e358bbe87a7629e0e424e3d1913d4a
TheFry <luke@archMac.localdomain>
2020-03-10 18:32:10 -0700

    Added a main function

---
+++ minget.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "minfs.h"
+
+uint32_t fs_base = 0;
+FILE *disk = NULL;
+struct superblock super_block;
+
+int main(int argc, char* argv[])
+{
+   printf("minget my ass\n");
+}
